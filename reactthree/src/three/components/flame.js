87dd3290cd905a3e350140326d39b9199b45cefd
keshanwen <3640@winnermedical.com>
2024-03-27 10:08:27 +0800

    feat: 火焰效果，序列帧动画

--- reactthree/src/three/components/flame.js
+++ reactthree/src/three/components/flame.js
@@ -1,45 +1,44 @@
 import * as THREE from 'three'
 
+// 矩形平面网格模型，用来渲染火焰的动画效果
+var w = 60;//火焰宽度  通过参数w可以快速调节火焰大小，以便于适应对应的三维场景
+var h = 1.6 * w;//火焰高度
+var geometry = new THREE.PlaneBufferGeometry(w, h); //矩形平面
+geometry.translate(0, h / 2, 0);//火焰底部中点和局部坐标系坐标原点重合
 
-// 矩形平面网格模型，用来渲染火焰动画效果
-const w = 60 // 火焰宽度 通过参数 w 可以快速调节火焰大小，以便于适应对应的三维场景
-const h = 1.6 * w // 火焰高度
-
-const geomerty = new THREE.PlaneBufferGeometry(w, h) // 矩形平面
-geomerty.translate(0, h / 2, 0) // 火焰底部中点和局部坐标系原点重合
-
-const textureLoader = new THREE.TextureLoader() // 纹理贴图加载器
-const texture = textureLoader.load('./火焰test.png') // 创建一个纹理对象
-const num = 15 // 火焰多少个帧图
-// .repeat 方法设置 uv 两个方向纹理重复数量
-texture.repeat.set(1 / num, 1) // 1 / num: 从图像上截图一帧火焰
-/*
-
-  texture.offest.x = 0 / num // 选择第1帧火焰
-  texture.offest.x = 1 / num // 选择第2帧火焰
-
-*/
-const material = new THREE.MeshBasicMaterial({
+var textureLoader = new THREE.TextureLoader();//纹理贴图加载器
+var texture = textureLoader.load('./火焰.png');//创建一个纹理对象
+var num = 15; //火焰多少帧图
+// .repeat方法设置uv两个方向纹理重复数量
+texture.repeat.set(1 / num, 1);// 1/num：从图像上截图一帧火焰
+// texture.offset.x = 0 / num;//选择第1帧火焰
+// texture.offset.x = 1 / num;//选择第2帧火焰
+// texture.offset.x = (num-1) / num;//选择第一帧火焰
+var material = new THREE.MeshBasicMaterial({
   map: texture,
-  transparent: true,
-  side: THREE.DoubleSide // 双面可见
-})
-const flame = new THREE.Mesh(geomerty, material) // 火焰网格模型
+ transparent: true,
+  opacity: 0.4,//整体调节透明度 和三维场景相融合
+  side: THREE.DoubleSide, //双面可见
+  depthWrite: false,//是否对深度缓冲区有任何的影响
+});
+var mesh = new THREE.Mesh(geometry, material);// 火焰网格模型
+var flame = new THREE.Group();//火焰组对象
+// 两个火焰mesh交叉叠加
+// flame.add(mesh, mesh.clone().rotateY(Math.PI / 2))
+// 四个火焰mesh交叉叠加
+flame.add(mesh, mesh.clone().rotateY(Math.PI / 2), mesh.clone().rotateY(Math.PI / 4), mesh.clone().rotateY(Math.PI / 4 * 3))
 
-let t = 0
 
+
+var t = 0;
+// 火焰动画生成
 function UpdateLoop() {
-  t += 0.1 // 调节火焰切换速度
-  if (t > num) t = 0
-  texture.offset.x = Math.floor(t) / num
-  window.requestAnimationFrame(UpdateLoop)
+  t += 0.1;//调节火焰切换速度
+  if (t > num) t = 0;
+  //  Math.floor(t)取整 保证一帧一帧切换
+  texture.offset.x = Math.floor(t) / num;// 动态更新纹理偏移 播放关键帧动画 产生火焰然后效果
+  window.requestAnimationFrame(UpdateLoop); //请求再次执行函数UpdateLoop
 }
-UpdateLoop()
+UpdateLoop();
 
-
-export {
-  flame
-}
-
-
-
+export { flame }
