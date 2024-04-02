6a73fff5f6b8c4f26fdd4515e3b741ba00d9e9b3
keshanwen <3640@winnermedical.com>
2024-04-02 18:10:56 +0800

    feat: 曲线Curve 简介

--- reactthree/src/three/study/curve/demo.js
+++ reactthree/src/three/study/curve/demo.js
@@ -1,46 +1,7 @@
-import * as THREE from 'three'
-
-
-const geometry = new THREE.BufferGeometry(); //创建一个几何体对象
-const R = 100; //圆弧半径
-const N = 50; //分段数量
-const sp = 2 * Math.PI / N;//两个相邻点间隔弧度
-// 批量生成圆弧上的顶点数据
-const arr = [];
-for (let i = 0; i < N; i++) {
-  const angle = sp * i;//当前点弧度
-  // 以坐标原点为中心，在XOY平面上生成圆弧上的顶点数据
-  const x = R * Math.cos(angle);
-  const y = R * Math.sin(angle);
-  arr.push(x, y, 0);
-}
-//类型数组创建顶点数据
-const vertices = new Float32Array(arr);
-// 创建属性缓冲区对象
-//3个为一组，表示一个顶点的xyz坐标
-const attribue = new THREE.BufferAttribute(vertices, 3);
-// 设置几何体attributes属性的位置属性
-geometry.attributes.position = attribue;
-
-// 线材质
-const material = new THREE.LineBasicMaterial({
-  color: 0xff0000 //线条颜色
+// 点材质
+const material = new THREE.PointsMaterial({
+  color: 0xffff00,
+  size: 10.0 //点对象像素尺寸
 });
-// 创建线模型对象   构造函数：Line、LineLoop、LineSegments
-// const line = new THREE.Line(geometry, material);
-const line = new THREE.LineLoop(geometry, material);//线条模型对象
-
-
-const R = 100; //圆弧半径
-const N = 50; //分段数量
-const sp = 2 * Math.PI / N;//两个相邻点间隔弧度
-// 设置圆心坐标
-const cx = 200;
-const cy = 100;
-for (let i = 0; i < N + 1; i++) {
-  const angle = sp * i;//当前点弧度
-  const x = cx + R * Math.cos(angle);
-  const y = cy + R * Math.sin(angle);
-  arr.push(x, y, 0);
-}
-
+// 点模型
+const points = new THREE.Points(geometry, material);
--- reactthree/src/three/study/curve/index.js
+++ reactthree/src/three/study/curve/index.js
@@ -1,6 +1,6 @@
 import * as THREE from 'three'
 
-const geometry = new THREE.BufferGeometry()
+/* const geometry = new THREE.BufferGeometry()
 const R = 100
 const N = 100
 const sp = 2 * Math.PI / N;
@@ -29,7 +29,38 @@ const material = new THREE.LineBasicMaterial({
 
 //const line = new THREE.Line(geometry, material)
 const line = new THREE.LineLoop(geometry, material)
+ */
+
+
+const geometry = new THREE.BufferGeometry()
+
+
+// const geometry = new THREE.Geometry()
+
+// 参数1和2表示椭圆中心坐标  参数3和4表示x和y方向半径
+const arc = new THREE.EllipseCurve(0, 0, 100, 50);
+// var arc = new THREE.ArcCurve(0, 0, 100, 0, 2 * Math.PI);
+var points = arc.getPoints(50); // 可以从曲线上获取顶点数据。获取点的方式并不是按照曲线等间距的方式，而是会考虑曲线斜率变化，斜率变化快的位置返回的顶点更密集。
+// var points = arc.getSpacedPoints(50); // 按照曲线长度等间距返回顶点数据
+
+
+
+geometry.setFromPoints(points);// 赋值给 geometry.attribute.position 属性
+
+const material = new THREE.LineBasicMaterial({
+  color: 0xff0000
+})
+
+const line = new THREE.Line(geometry, material)
+
+const materials = new THREE.PointsMaterial({
+  color: 0xff0000,
+  size: 4.0 //点对象像素尺寸
+})
+
+const point = new THREE.Points(geometry,materials)
 
 export {
-  line
+  line,
+  point
 }
