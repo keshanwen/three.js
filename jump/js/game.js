53956cc9f684434c13dcb1732741271c62f18ada
keshanwen <3640@winnermedical.com>
2024-04-03 15:45:22 +0800

    feat: 跳一跳

--- jump/js/game.js
+++ jump/js/game.js
@@ -7,7 +7,7 @@ class Game {
       cubeWidth: 4,
       cubeHeight: 2,
       cubeDeep: 4,
-      jumperColor: 0x232323,
+      jumperColor: 'blue',
       jumperWidth: 1,
       jumperHeight: 2,
       jumperDeep: 1,
@@ -19,8 +19,8 @@ class Game {
       current: new THREE.Vector3(0, 0, 0),
       next: new THREE.Vector3(0, 0, 0),
     };
-    this.renderer = new THREE.WebGLRenderer({antialias: true});
-    this.size = {width: window.innerWidth, height: window.innerHeight};
+    this.renderer = new THREE.WebGLRenderer({ antialias: true });
+    this.size = { width: window.innerWidth, height: window.innerHeight };
     this.cubes = [];
     this.cubeStat = {
       nextDir: "",
@@ -41,7 +41,7 @@ class Game {
   }
 
   init() {
-    this._setAxesHelper()
+    this._setAxes()
     this._setCamera();
     this._setRenderer();
     this._setLight();
@@ -82,24 +82,142 @@ class Game {
   };
 
   _handleMouseDown() {
-
+    if (!this.jumperStat.ready && this.jumper.scale.y > 0.02) {
+      this.jumper.scale.y -= 0.01;
+      this.jumperStat.xSpeed += 0.004;
+      this.jumperStat.ySpeed += 0.008;
+      this._render();
+      requestAnimationFrame(() => {
+        this._handleMouseDown()
+      })
+    }
   };
 
   _handleMouseUp() {
-
+    this.jumperStat.ready = true;
+    if (this.jumper.position.y >= 1) {
+      if (this.jumper.scale.y < 1) {
+        this.jumper.scale.y += 0.1;
+      }
+      if (this.cubeStat.nextDir == "left") {
+        this.jumper.position.x -= this.jumperStat.xSpeed;
+      } else {
+        this.jumper.position.z -= this.jumperStat.xSpeed;
+      }
+      this.jumper.position.y += this.jumperStat.ySpeed;
+      this.jumperStat.ySpeed -= 0.01;
+      this._render();
+      requestAnimationFrame(() => {
+        this._handleMouseUp();
+      })
+    } else {
+      this.jumperStat.ready = false;
+      this.jumperStat.xSpeed = 0;
+      this.jumperStat.ySpeed = 0;
+      this.jumper.position.y = 1;
+      this.jumper.scale.y = 1;
+      this._checkInCube();
+      if (this.falledStat.location == 1) {
+        this.score++;
+        this._createCube();
+        this._updateCamera();
+        if (this.successCallback) {
+          this.successCallback(this.score);
+        }
+      } else {
+        this._falling()
+      }
+    }
   };
 
   _checkInCube() {
-
+    let distanceCur, distanceNext;
+    let should = (this.config.jumperWidth + this.config.cubeWidth) / 2;
+    if (this.cubeStat.nextDir == "left") {
+      distanceCur = Math.abs(this.jumper.position.x - this.cubes[this.cubes.length - 2].position.x);
+      distanceNext = Math.abs(this.jumper.position.x - this.cubes[this.cubes.length - 1].position.x);
+    } else {
+      distanceCur = Math.abs(this.jumper.position.z - this.cubes[this.cubes.length - 2].position.z);
+      distanceNext = Math.abs(this.jumper.position.z - this.cubes[this.cubes.length - 1].position.z);
+    }
+    if (distanceCur < should) {
+      this.falledStat.distance = distanceCur;
+      this.falledStat.location = distanceCur < this.config.cubeWidth / 2 ? -1 : -10;
+    } else if (distanceNext < should) {
+      this.falledStat.distance = distanceNext;
+      this.falledStat.location = distanceNext < this.config.cubeWidth / 2 ? 1 : 10;
+    } else {
+      this.falledStat.location = 0;
+    }
   };
 
   _falling() {
-
+    if (this.falledStat.location == 10) {
+      if (this.cubeStat.nextDir == "left") {
+        if (this.jumper.position.x > this.cubes[this.cubes.length - 1].position.x) {
+          this._fallingRotate("leftBottom")
+        } else {
+          this._fallingRotate("leftTop")
+        }
+      } else {
+        if (this.jumper.position.z > this.cubes[this.cubes.length - 1].position.z) {
+          this._fallingRotate("rightBottom")
+        } else {
+          this._fallingRotate("rightTop")
+        }
+      }
+    } else if (this.falledStat.location == -10) {
+      if (this.cubeStat.nextDir == "left") {
+        this._fallingRotate("leftTop")
+      } else {
+        this._fallingRotate("rightTop")
+      }
+    } else if (this.falledStat.location == 0) {
+      this._fallingRotate("none")
+    }
   };
 
   _fallingRotate(dir) {
-
-
+    let offset = this.falledStat.distance - this.config.cubeWidth / 2;
+    let rotateAxis = dir.includes("left") ? 'z' : "x";
+    let rotateAdd = this.jumper.rotation[rotateAxis] + 0.1;
+    let rotateTo = this.jumper.rotation[rotateAxis] < Math.PI / 2;
+    let fallingTo = this.config.ground + this.config.jumperWidth / 2 + offset;
+    if (dir === 'rightTop') {
+      rotateAdd = this.jumper.rotation[rotateAxis] - 0.1;
+      rotateTo = this.jumper.rotation[rotateAxis] > -Math.PI / 2;
+    } else if (dir === 'rightBottom') {
+      rotateAdd = this.jumper.rotation[rotateAxis] + 0.1;
+      rotateTo = this.jumper.rotation[rotateAxis] < Math.PI / 2;
+    } else if (dir === 'leftBottom') {
+      rotateAdd = this.jumper.rotation[rotateAxis] - 0.1;
+      rotateTo = this.jumper.rotation[rotateAxis] > -Math.PI / 2;
+    } else if (dir === 'leftTop') {
+      rotateAdd = this.jumper.rotation[rotateAxis] + 0.1;
+      rotateTo = this.jumper.rotation[rotateAxis] < Math.PI / 2;
+    } else if (dir === 'none') {
+      rotateTo = false;
+      fallingTo = this.config.ground;
+    } else {
+      throw Error('Arguments Error')
+    }
+    if (!this.fallingStat.end) {
+      if (rotateTo) {
+        this.jumper.rotation[rotateAxis] = rotateAdd
+      } else if (this.jumper.position.y > fallingTo) {
+        this.jumper.position.y -= 0.2;
+      } else {
+        this.fallingStat.end = true;
+      }
+      this._render();
+      requestAnimationFrame(() => {
+        this._falling()
+      })
+    } else {
+      if (this.failedCallback) {
+        this.failedCallback()
+      }
+    }
   };
 
   _setCamera() {
@@ -123,7 +241,7 @@ class Game {
 
   _createCube() {
     let geometry = new THREE.CubeGeometry(this.config.cubeWidth, this.config.cubeHeight, this.config.cubeDeep);
-    let material = new THREE.MeshLambertMaterial({color: this.config.cubeColor});
+    let material = new THREE.MeshLambertMaterial({ color: this.config.cubeColor });
     let cube = new THREE.Mesh(geometry, material);
     if (this.cubes.length) {
       cube.position.x = this.cubes[this.cubes.length - 1].position.x;
@@ -148,7 +266,7 @@ class Game {
 
   _createJumper() {
     let geometry = new THREE.CubeGeometry(this.config.jumperWidth, this.config.jumperHeight, this.config.jumperDeep);
-    let material = new THREE.MeshLambertMaterial({color: this.config.jumperColor});
+    let material = new THREE.MeshLambertMaterial({ color: this.config.jumperColor });
     this.jumper = new THREE.Mesh(geometry, material);
     this.jumper.position.y = 1;
     geometry.translate(0, 1, 0);
@@ -205,12 +323,24 @@ class Game {
     this.renderer.render(this.scene, this.camera);
   };
 
-  _restart(){
-
+  _restart() {
+    this.cameraPros = { current: new THREE.Vector3(0, 0, 0), next: new THREE.Vector3() };
+    this.fallingStat = { end: false, speed: 0.2 };
+    let length = this.cubes.length;
+    this.scene.remove(this.jumper);
+    for (let i = 0; i < length; i++) {
+      this.scene.remove(this.cubes.shift());
+    }
+    this.score = 0;
+    this.successCallback(this.score);
+    this._createCube();
+    this._createCube();
+    this._createJumper();
+    this._updateCamera();
   };
 
-  _setAxesHelper() {
-    // AxesHelper 辅助观察的坐标系
+  _setAxes() {
+    // AxesHelper：辅助观察的坐标系
     const axesHelper = new THREE.AxesHelper(150);
     this.scene.add(axesHelper)
   }
