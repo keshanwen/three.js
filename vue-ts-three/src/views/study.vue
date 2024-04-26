8f02c9fc638d35da10537d61229346185ee9e98e
keshanwen <3640@winnermedical.com>
2024-04-26 09:07:30 +0800

    feat: 增加贴图

---
+++ vue-ts-three/src/views/study.vue
@@ -0,0 +1,32 @@
+<template>
+  <div ref="containerRef" class="about-home">
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted, onUnmounted } from 'vue';
+import CreateThree from '@/threeEdit/createThreeInstance';
+import * as THREE from 'three';
+import { setFloorHook } from '@/threeEdit/util/setScene'
+import { model } from '@/threeEdit/util/flyLine'
+
+let containerRef = ref()
+let app = new CreateThree({
+  helperBool: true,
+  logPosTargetBool: true
+});
+
+  // const { gridHelp, mesh } = setFloorHook()
+  // app.scene.add(model)
+
+onMounted(() => {
+  app.append(containerRef.value);
+});
+
+</script>
+
+<style>
+.about-home {
+  height: 100vh;
+}
+</style>
