e5ba47dd612c04285c1100e88e36c6e7cd315f7a
keshanwen <3640@winnermedical.com>
2024-06-26 08:43:43 +0800

    feat: webGl 资料

--- vue-ts-three/src/router/index.ts
+++ vue-ts-three/src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-// import HomeView from '../views/HomeView.vue'
+import HomeView from '../views/HomeView.vue'
 import Study from '@/views/study.vue'
 
 const router = createRouter({
@@ -8,7 +8,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: Study,
+      component: HomeView,
     },
     {
       path: '/about',
